9baf0c08372412d801e00c1a2998f231b25352dc
boilingSugar <1418068612@qq.com>
2022-04-27 10:47:34 +0800

    [updata]topo-2D多个详情展示失败原因

--- src/pages/Topo/Topo-2D.vue
+++ src/pages/Topo/Topo-2D.vue
@@ -5,9 +5,13 @@
 <script>
 export default {
 
+// 不可以这样的原因是，创建群组子节点时，class为detail已经生成，但detailBox还没有，无法将新的子节点添加进去，最后全部堆积在child的最后一位里面
 }
 </script>
 
 <style lang="less" scoped>
 .topo-2d{}
 </style>
+
+
+
